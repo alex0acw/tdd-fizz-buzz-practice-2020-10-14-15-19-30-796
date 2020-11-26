e366180eae76c6e022205339a7b27f68088abded
AUAL <alex.au@oocl.com>
2020-11-26 13:59:26 +0800

    [FizzBuzz][Alex Au] feat: implemented countOff for multiple of 7 (Whizz)

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,12 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     private static final int FIZZ_MULTIPLE = 3;
     private static final int BUZZ_MULTIPLE = 5;
+    private static final int WHIZZ_MULTIPLE = 7;
 
     public String countOff(int number) {
         if (number % FIZZ_MULTIPLE == 0) return "Fizz";
         if (number % BUZZ_MULTIPLE == 0) return "Buzz";
+        if (number % WHIZZ_MULTIPLE == 0) return "Whizz";
         return String.valueOf(number);
     }
 }
