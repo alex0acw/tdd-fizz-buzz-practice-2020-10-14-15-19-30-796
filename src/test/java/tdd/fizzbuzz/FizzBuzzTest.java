0ef828c35393542b7217122ccde94879f639cdd9
AUAL <alex.au@oocl.com>
2020-11-26 13:49:05 +0800

    [FizzBuzz][Alex Au] test: implemented test case for normal number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_normal_number_when_normal_number() {
+        //
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //
+        String actual = fizzBuzz.countOff(1);
+        //
+        assertEquals("1", actual);
+    }
 }
