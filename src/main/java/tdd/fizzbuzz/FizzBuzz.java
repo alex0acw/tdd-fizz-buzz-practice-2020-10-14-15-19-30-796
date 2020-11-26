25e5014ffa1fcd3a9b320d45e9a64b4cfb327a40
AUAL <alex.au@oocl.com>
2020-11-26 14:21:52 +0800

    [FizzBuzz][Alex Au] feat: modify countOff to yield Fizz if incoming value's string contains '3'

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,11 +1,13 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    private static final String FIZZ_TRIGGER_CHARACTER = "3";
     private static final int FIZZ_MULTIPLE = 3;
     private static final int BUZZ_MULTIPLE = 5;
     private static final int WHIZZ_MULTIPLE = 7;
 
     public String countOff(int number) {
+        if (String.valueOf(number).contains(FIZZ_TRIGGER_CHARACTER)) return "Fizz";
         StringBuilder result = new StringBuilder();
         if (number % FIZZ_MULTIPLE == 0) result.append("Fizz");
         if (number % BUZZ_MULTIPLE == 0) result.append("Buzz");
