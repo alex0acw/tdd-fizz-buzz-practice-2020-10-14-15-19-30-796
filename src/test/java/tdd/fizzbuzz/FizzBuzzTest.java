452c6d86ec9e11c241bc1d08afc7247d5fc00757
AUAL <alex.au@oocl.com>
2020-11-26 13:53:47 +0800

    [FizzBuzz][Alex Au] test: implemented test case for multiple of 3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -7,11 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
     @Test
     void should_return_normal_number_when_normal_number() {
-        //
+        //given
         FizzBuzz fizzBuzz = new FizzBuzz();
-        //
+        //when
         String actual = fizzBuzz.countOff(1);
-        //
+        //then
         assertEquals("1", actual);
+    }
+
+    @Test
+    void should_return_fizz_when_multiple_of_3() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String actual = fizzBuzz.countOff(12);
+        //then
+        assertEquals("Fizz", actual);
     }
 }
