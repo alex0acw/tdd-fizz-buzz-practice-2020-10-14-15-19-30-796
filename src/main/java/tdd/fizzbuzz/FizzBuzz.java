da5e8febf8be9380899ef8f4a948ceee254bcbe8
AUAL <alex.au@oocl.com>
2020-11-26 13:55:02 +0800

    [FizzBuzz][Alex Au] feat: implemented countOff for multiple of 3 (FIZZ)

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,10 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    private static final int FIZZ_MULTIPLE = 3;
+
     public String countOff(int number) {
+        if (number % FIZZ_MULTIPLE == 0) return "Fizz";
         return String.valueOf(number);
     }
 }
