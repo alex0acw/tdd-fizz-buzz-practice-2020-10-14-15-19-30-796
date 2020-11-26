b44878c73cbbe03e76112fdf888310324ee968d7
AUAL <alex.au@oocl.com>
2020-11-26 14:02:09 +0800

    [FizzBuzz][Alex Au] test: implemented test case for multiple of 3&5 (FizzBuzz)

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -7,41 +7,56 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
     @Test
     void should_return_normal_number_when_normal_number() {
-        //given
         FizzBuzz fizzBuzz = new FizzBuzz();
+        //given
+        int number = 1;
         //when
-        String actual = fizzBuzz.countOff(1);
+        String actual = fizzBuzz.countOff(number);
         //then
         assertEquals("1", actual);
     }
 
     @Test
     void should_return_fizz_when_multiple_of_3() {
-        //given
         FizzBuzz fizzBuzz = new FizzBuzz();
+        //given
+        int number = 12;
         //when
-        String actual = fizzBuzz.countOff(12);
+        String actual = fizzBuzz.countOff(number);
         //then
         assertEquals("Fizz", actual);
     }
 
     @Test
     void should_return_fizz_when_multiple_of_5() {
-        //given
         FizzBuzz fizzBuzz = new FizzBuzz();
+        //given
+        int number = 5;
         //when
-        String actual = fizzBuzz.countOff(5);
+        String actual = fizzBuzz.countOff(number);
         //then
         assertEquals("Buzz", actual);
     }
 
     @Test
     void should_return_fizz_when_multiple_of_7() {
-        //given
         FizzBuzz fizzBuzz = new FizzBuzz();
+        //given
+        int number = 7;
         //when
         String actual = fizzBuzz.countOff(7);
         //then
         assertEquals("Whizz", actual);
+    }
+
+    @Test
+    void should_return_fizzbuzz_when_multiple_of_3_and_5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //given
+        int number = 15;
+        //when
+        String actual = fizzBuzz.countOff(15);
+        //then
+        assertEquals("FizzBuzz", actual);
     }
 }
